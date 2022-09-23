74488c54fe4818612ae012e970f9d9eb55f5834a
Rebecca Sansale <32822979+rebexxxxxx@users.noreply.github.com>
2022-09-23 15:36:57 -0400

    Add files via upload

--- two_parallel_mediators_sc_ui.R
+++ two_parallel_mediators_sc_ui.R
@@ -1,45 +1,54 @@
-#two parallel mediator variables std coef ui
 list(
   withTags(
-    table(style = "width: 150px;", id = "STDpath_table",
+    table(style = "width: 350px;", id = "STDpath_table",
           tr(
-            td(style="padding-top:0px;padding-left:10px;width:32px;", label("a1")),  
-            td(textInput(inputId = "STa1", label = NULL, value = "0.00"))
+            td(style="padding-top:0px;padding-left:10px;width:32px;", label("a1 low")),  
+            td(textInput(inputId = "STa1_low", label = NULL, value = "0.14")),
+            td(style="padding-top:0px;padding-left:10px;width:32px;", label("a1 high")),  
+            td(textInput(inputId = "STa1_high", label = NULL, value = "0.39"))
           ),
           tr(
-            td(style="padding-top:0px;padding-left:10px;width:32px;", label("a2")),  
-            td(textInput(inputId = "STa2", label = NULL, value = "0.00"))
+            td(style="padding-top:0px;padding-left:10px;width:32px;", label("a2 low")),  
+            td(textInput(inputId = "STa2_low", label = NULL, value = "0.14")),
+            td(style="padding-top:0px;padding-left:10px;width:32px;", label("a2 high")),  
+            td(textInput(inputId = "STa2_high", label = NULL, value = "0.39"))
           ),
           tr(
-            td(style="padding-top:0px;padding-left:10px;width:32px;", label("b1")),  
-            td(textInput(inputId = "STb1", label = NULL, value = "0.00"))
+            td(style="padding-top:0px;padding-left:10px;width:32px;", label("b1 low")),  
+            td(textInput(inputId = "STb1_low", label = NULL, value = "0.14")),
+            td(style="padding-top:0px;padding-left:10px;width:32px;", label("b1 high")),  
+            td(textInput(inputId = "STb1_high", label = NULL, value = "0.39"))
           ),
           tr(
-            td(style="padding-top:0px;padding-left:10px;width:32px;", label("b2")),  
-            td(textInput(inputId = "STb2", label = NULL, value = "0.00"))
+            td(style="padding-top:0px;padding-left:10px;width:32px;", label("b2 low")),  
+            td(textInput(inputId = "STb2_low", label = NULL, value = "0.14")),
+            td(style="padding-top:0px;padding-left:10px;width:32px;", label("b2 high")),  
+            td(textInput(inputId = "STb2_high", label = NULL, value = "0.39"))
           ),
           tr(
-            td(style="padding-top:0px;padding-left:10px;width:32px;", label("c'")),  
-            td(textInput(inputId = "STcprime", label = NULL, value = "0.00"))
+            td(style="padding-top:0px;padding-left:10px;width:32px;", label("c low")),  
+            td(textInput(inputId = "STc_low", label = NULL, value = "0.14")),
+            td(style="padding-top:0px;padding-left:10px;width:32px;", label("c high")),  
+            td(textInput(inputId = "STc_high", label = NULL, value = "0.39"))
           ),
           tr(
-            td(style="padding-top:0px;padding-left:10px;width:32px;", label("r")),  
-            td(textInput(inputId = "cor32", label = NULL, value = "0.00"))
+            td(style="padding-top:0px;padding-left:10px;width:32px;", label("rM1M2")),  
+            td(textInput(inputId = "cor32", label = NULL, value = "0.3"))
           ),
           tr(
-            td(style="padding-top:0px;padding-left:10px;width:32px;", label("std. Deviation: X")),  
+            td(style="padding-top:0px;padding-left:10px;width:32px;", label("std. dev: X")),  
             td(textInput(inputId = "SDX", label = NULL, value = "1.00"))
           ),
           tr(
-            td(style="padding-top:0px;padding-left:10px;width:32px;", label("std. Deviation: M1")),  
+            td(style="padding-top:0px;padding-left:10px;width:32px;", label("std. dev: M1")),  
             td(textInput(inputId = "SDM1", label = NULL, value = "1.00"))
           ),
           tr(
-            td(style="padding-top:0px;padding-left:10px;width:32px;", label("std. Deviation: M2")),  
+            td(style="padding-top:0px;padding-left:10px;width:32px;", label("std. dev: M2")),  
             td(textInput(inputId = "SDM2", label = NULL, value = "1.00"))
           ),
           tr(
-            td(style="padding-top:0px;padding-left:10px;width:32px;", label("std. Deviation: Y")),  
+            td(style="padding-top:0px;padding-left:10px;width:32px;", label("std. dev: Y")),  
             td(textInput(inputId = "SDY", label = NULL, value = "1.00"))
           )
     )  
